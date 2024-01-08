4ab5eb1f4722a6c77089f4b6df87083e7af8767f
Андрей Шульга <koreni2000@gmail.com>
2024-01-08 17:12:08 +0200

    Change block

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,12 @@
-terraform {
-  required_version = ">= 0.13"
-  required_providers {
-    yandex = {
-      source = "yandex-cloud/yandex"
-      version = "0.104.0"
-    }
-  }
-}
+# terraform {
+#   required_version = ">= 0.13"
+#   required_providers {
+#     yandex = {
+#       source = "yandex-cloud/yandex"
+#       version = "0.104.0"
+#     }
+#   }
+# }
 
 provider "yandex" {
   token     = var.token
